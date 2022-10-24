52088d36ff3acd75ebcc394f2d970fa6745a04d6
Erick <erickjesus00001@gmail.com>
2022-10-24 17:38:47 -0500

    "Hice pruebas, y agregué comentarios a conexión

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "db4free.net:3306/universidad:proy";
+$servername = "db4free.net";
 $database = "universidad_proy";
 $username = "gatitolover01";
-$password = "equipoGl01";
+$password = "EquipoGl01";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
@@ -10,5 +10,11 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+/*NOOOOOOOOOOOOOOOOOOOOOOOOOO BORRAR
+if ($result = mysqli_query($conn, "SELECT * FROM profesores")) {
+    echo "Returned rows are: " . mysqli_num_rows($result);
+    // Free result set
+    mysqli_free_result($result);
+  }  */
 mysqli_close($conn);
 ?>
